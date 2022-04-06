9b44d17173da997e03571efae9d1ab48005c4a4a
Jamie Fletcher <j591fletcher@gmail.com>
2022-04-06 18:55:30 -0400

    Selection sort complete

--- selection.py
+++ selection.py
@@ -1,2 +1,17 @@
 #selection sort
 
+def selection(array):
+    n = len(array)
+
+    for i in range(n):
+        min = i
+        for j in range(i+1, n):
+            if array[min] > array[j]:
+                min = j
+        array[i], array[min] = array[min], array[i]
+
+
+test = [9,2,3,2,5,1213,22,3,4,234234,343,23,8876,9]
+selection(test)
+for i in range(len(test)):
+    print (test[i], end=" ")
