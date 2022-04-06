b08311a7ab2d7e52b0077c023c601854b5f5cc1f
Jamie Fletcher <j591fletcher@gmail.com>
2022-04-06 18:04:32 -0400

    Bubble sort algorithm

--- bubblesort.py
+++ bubblesort.py
@@ -1,1 +1,17 @@
 #bubblesort
+
+def bubble(arr):
+    length = len(arr)
+    for i in range(length):
+        for j in range(0,length-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                 
+
+
+test = [1,9,3,2,4]
+bubble(test)
+
+for i in range(len(test)):
+    print (test[i], end=" ")
+
