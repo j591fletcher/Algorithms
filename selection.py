88171bb714864d04641393a0e2fc8b9c70dcc053
Jamie Fletcher <j591fletcher@gmail.com>
2022-04-06 18:59:50 -0400

    added test case to selection sort

--- selection.py
+++ selection.py
@@ -12,6 +12,11 @@ def selection(array):
 
 
 test = [9,2,3,2,5,1213,22,3,4,234234,343,23,8876,9]
+sameTest = [1,1,1,1,1]
 selection(test)
+selection(sameTest)
 for i in range(len(test)):
     print (test[i], end=" ")
+print("\n")
+for i in range(len(sameTest)):
+    print (sameTest[i], end=" ")
